b396fd7a009e7e30b1c2ba32da9c14af8c5c29a2
David Landes <dlandes.develop@outlook.com>
2021-02-11 18:17:08 -0500

    Add line limits, etc..

--- simulation.py
+++ simulation.py
@@ -2,41 +2,6 @@ import simpy
 import simpy.events
 import random
 
-# StationQueue class represents the line of a station in a Restaurant's drive thru. Certain queues can only hold a maximum number of customers.
-#     Stores a first-in first-out line of Customer objects.
-#            env: simpy simulation environment.
-#        maxSize: maximum number of customers that will fit in this queue.
-class StationQueue:
-    def __init__(self, env, maxSize):
-        self.env = env
-        self.maxSize = maxSize
-        self.queue = []
-
-    # Push a customer to the back of the queue. 
-    # Note: ONLY accepts Customer objects. Will not push customer if the queue is full.
-    # Return True if customer was successfully added.
-    def enqueue(self, customer):
-        if (type(customer) == Customer and self.length() < self.maxSize):
-            self.queue.append(customer)
-            return True
-        else:
-            return False
-    
-    # Remove a customer from the front of the queue.
-    # Return True if customer was successfully removed.
-    def dequeue(self):
-        if (len(self.queue) > 0):
-            self.queue.remove(0)
-            return True
-        else:
-            return False
-
-    # Return the current number of customers waiting in this queue.
-    def length(self):
-        return len(self.queue)
-
-
-
 
 # Restaurant class represents a Restaurant's drive thru process.
 #            env: simpy simulation environment.
@@ -48,20 +13,22 @@ class Restaurant:
     NUM_OF_ORDER_STATIONS = 1
     NUM_OF_PAY_STATIONS = 1
     NUM_OF_PICKUP_STATIONS = 1
+
+    # Action times in minutes.
     meanOrderTime = 3.0
     meanFoodPrepTime = 6.0
     meanPayTime = 2.0
     meanPickupTime = 2.0
-    # Rate x / y where  x = number of customers for every y units of time.
-    customerArrivalRate = 1.0 / 2.0
+
+    # Rate x / y where  x = number of customers for every y minutes.
+    customerArrivalRate = 5.0 / 1.0
 
     def __init__(self, env, orderStation, payStation, pickupStation):
         self.env = env
-
-        # Queues for stations.
-        self.orderQueue = StationQueue(env, 7)
-        self.payQueue = StationQueue(env, 4)
-        self.pickupQueue = StationQueue(env, 1)
+        self.customerList = []
+        self.totalCustomers = 0
+        self.numCustomersLeft = 0
+        self.numCustomersStayed = 0
 
         # Stations.
         self.orderStation = orderStation
@@ -73,12 +40,38 @@ class Restaurant:
         for c in range(numOfCustomers):
             # Generate customer.
             newCustomer = Customer(self.env, self)
-           # c = newCustomer.begin
+
+            self.customerList.append(newCustomer)
+
+            self.totalCustomers += 1
             env.process(newCustomer.start())
 
             # Customer arrives every x minutes
             t = random.expovariate(self.customerArrivalRate)
             yield env.timeout(t)
+    
+    # Average time a customer waited in the drive thru. Returns time in minutes.
+    # NOTE: Only call this function after the simulation has finished.
+    def calculateAverageDriveThruTime(self):
+        averageTime = 0.0
+
+        # customerList contains ALL potential customers, including those who entered the line AND those who left early..
+        for person in self.customerList:
+            if person.enterTime != -1.0 and person.exitTime != -1.0:
+                timeSpent = person.exitTime - person.enterTime
+                averageTime += timeSpent
+        
+        averageTime = float(averageTime / self.numCustomersStayed)
+        return averageTime
+
+
+    def printStats(self):
+        print("--------------------------------------- Stats ---------------------------------------")
+        print(f"{self.totalCustomers} potential customers..")
+        print(f"{self.numCustomersLeft} customers left..")
+        print(f"{self.numCustomersStayed} customers entered the line..\n")
+        print(f"Average time spent in drive thru: {self.calculateAverageDriveThruTime()} minutes..")
+        print("-------------------------------------------------------------------------------------")
 
 
 
@@ -86,82 +79,122 @@ class Restaurant:
 #            env: simpy simulation environment.
 #     restaurant: object of Restaurant class.
 class Customer:
-    # TODO: prevent 
+    
+    # If true, print customer events to the console.
+    isEventStampingOn = True
     customerNumber = 0
 
     def __init__(self, env, restaurant):
         self.env = env
         self.restaurant = restaurant
+
         Customer.customerNumber += 1
         self.number = Customer.customerNumber
 
+        # Track enter/exit in simulation time. 
+        # Note: If the customer leaves because the line is too long, these values will remain -1.0
+        self.enterTime = -1.0
+        self.exitTime = -1.0
+
     # Start the simulation of customer going through drive thru line.
     def start(self):
-        self.event_stamp(f"Customer {self.number} enters the line.")
-        # Wait for an open order station.
-        order = self.restaurant.orderStation.request()
-        yield order
+        # Enter the drive thru if there is enough space. Max of 7 customers in line plus the 1 at the order station.
+        if (len(self.restaurant.orderStation.queue) <= 8):
 
-        # Enter the order station.
-        self.event_stamp(f"Customer {self.number} is ordering.")
-        delay = random.weibullvariate((1 / Restaurant.meanOrderTime), 1.5)
-        orderTime = simpy.events.Timeout(env, delay)
-        yield orderTime 
+            self.event_stamp(f"Customer {self.number} enters the line. {len(self.restaurant.orderStation.queue)} customers in order line.")
+            self.enterTime = env.now
+            self.restaurant.numCustomersStayed += 1
 
-        # Finished ordering, leave the order station.
-        self.restaurant.orderStation.release(order)
+            # Wait for an open order station.
+            order = self.restaurant.orderStation.request()
+            yield order
 
-        # Start food prep.
-        prepTimeDelay = random.weibullvariate((1 / Restaurant.meanFoodPrepTime), 2.0)
-        prepTime = simpy.events.Timeout(env, prepTimeDelay)
+            # Enter the order station.
+            self.event_stamp(f"Customer {self.number} is ordering.")
+            delay = random.weibullvariate((1 / Restaurant.meanOrderTime), 1.5)
+            orderTime = simpy.events.Timeout(env, delay)
+            yield orderTime 
+
+            # Start food prep.
+            prepTimeDelay = random.weibullvariate((1 / Restaurant.meanFoodPrepTime), 2.0)
+            prepTime = simpy.events.Timeout(env, prepTimeDelay)
+
+            # Wait until there is enough space to move forward. Max 4 between order and pay station, plus 1 in the pay station.
+            if (len(self.restaurant.payStation.queue) >= 5):
+                self.event_stamp("pay station full... waiting...")
+                yield self.restaurant.payStation.queue[0]
+
+            # Finished ordering, leave the order station.
+            self.restaurant.orderStation.release(order)
 
 
-        # Wait for an open pay station.
-        pay = self.restaurant.payStation.request()
-        yield pay
+            # Wait for an open pay station.
+            pay = self.restaurant.payStation.request()
+            yield pay
+            
+            # Enter the pay station.
+            self.event_stamp(f"Customer {self.number} is paying. {len(self.restaurant.payStation.queue)} customers in pay line.")
+            delay = random.weibullvariate((1 / Restaurant.meanPayTime), 1.5)
+            payTime = simpy.events.Timeout(env, delay)
+            yield payTime
+
+            # Wait until there is enough space to move forward. Max 1 between pay and pickup station, plus 1 in the pickup station.
+            if (len(self.restaurant.pickupStation.queue) >= 2):
+                self.event_stamp("pickup station full... waiting...")
+                yield self.restaurant.pickupStation.queue[0]
+
+            # Finished paying, leave the pay station.
+            self.restaurant.payStation.release(pay)
+
+
+            # Wait for an open pickup station.
+            pickup = self.restaurant.pickupStation.request()
+            yield pickup
+
+            # Enter the pickup station.
+            self.event_stamp(f"Customer {self.number} is picking up. {len(self.restaurant.pickupStation.queue)} customers in pickup line.")
+            delay = random.weibullvariate((1 / Restaurant.meanPickupTime), 1.5)
+            pickupTime = simpy.events.Timeout(env, delay)
+            yield prepTime
+            yield pickupTime
+
+            # Finished picking up items, leave the pickup station.
+            self.restaurant.pickupStation.release(pickup)
+            self.event_stamp(f"Customer {self.number} exits the line.")
+            self.exitTime = env.now
+        else:
+            self.event_stamp(f"Line too long. Customer {self.number} left.")
+            self.restaurant.numCustomersLeft += 1
+        return 1
+
         
-        # Enter the pay station.
-        self.event_stamp(f"Customer {self.number} is paying.")
-        delay = random.weibullvariate((1 / Restaurant.meanPayTime), 1.5)
-        payTime = simpy.events.Timeout(env, delay)
-        yield payTime
-
-        # Finished paying, leave the pay station.
-        self.restaurant.payStation.release(pay)
-
-
-        # Wait for an open pickup station.
-        pickup = self.restaurant.pickupStation.request()
-        yield pickup
-
-        # Enter the pickup station.
-        self.event_stamp(f"Customer {self.number} is picking up.")
-        delay = random.weibullvariate((1 / Restaurant.meanPickupTime), 1.5)
-        pickupTime = simpy.events.Timeout(env, delay)
-        yield prepTime
-        yield pickupTime
-
-        # Finished picking up items, leave the pickup station.
-        self.restaurant.pickupStation.release(pickup)
-        self.event_stamp(f"Customer {self.number} exits the line.")
-
-    
     def event_stamp(self, eventMessage):
-        print(f"{self.env.now} : {eventMessage}")
+        if self.isEventStampingOn:
+            # print(f"Order line: {len(self.restaurant.orderStation.queue)}\nPay line: {len(self.restaurant.payStation.queue)}\nPickup line: {len(self.restaurant.pickupStation.queue)}\n\n")
+            print(f"{self.env.now} : {eventMessage}")
 
 
-random.seed(123456)
+#random.seed(123456)
+
+Customer.isEventStampingOn = False
+
 
 # Create the simulation environment.
 env = simpy.Environment()
 
-# Establish resources.
+
+# Establish restaurant resources.
 orderStation = simpy.Resource(env, capacity=Restaurant.NUM_OF_ORDER_STATIONS)
 payStation = simpy.Resource(env, capacity=Restaurant.NUM_OF_PAY_STATIONS)
 pickupStation = simpy.Resource(env, capacity=Restaurant.NUM_OF_PICKUP_STATIONS)
 
-restuarant = Restaurant(env, orderStation, payStation, pickupStation)
-customers = restuarant.generate_customers(10)
+# Generate the restaurant and the customers.
+restaurant = Restaurant(env, orderStation, payStation, pickupStation)
+customers = restaurant.generate_customers(500000000)
 env.process(customers)
 
-env.run()
+# Run for 120 minutes : 2 hours.
+env.run(120)
+
+
+restaurant.printStats()
